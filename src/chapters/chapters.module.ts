2b42abc01daad0661e2535164d2504da84d03cc0
Zin Lin Khant <zlkzlkzlk1122@gmail.com>
2024-05-25 14:52:23 +0630

    25 may

--- src/chapters/chapters.module.ts
+++ src/chapters/chapters.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ChaptersService } from './chapters.service';
 import { ChaptersController } from './chapters.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Chapter } from './entities/chapter.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Chapter])],
   controllers: [ChaptersController],
   providers: [ChaptersService],
+  exports:[ChaptersService]
 })
 export class ChaptersModule {}
--- src/chapters/chapters.service.ts
+++ src/chapters/chapters.service.ts
@@ -1,26 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateChapterDto } from './dto/create-chapter.dto';
 import { UpdateChapterDto } from './dto/update-chapter.dto';
+import { Chapter } from './entities/chapter.entity';
 
 @Injectable()
 export class ChaptersService {
-  create(createChapterDto: CreateChapterDto) {
-    return 'This action adds a new chapter';
+  constructor(
+    @InjectRepository(Chapter)
+    private chaptersRepository: Repository<Chapter>,
+  ) {}
+
+  async create(createChapterDto: CreateChapterDto): Promise<Chapter> {
+    const chapter = this.chaptersRepository.create(createChapterDto);
+    return this.chaptersRepository.save(chapter);
   }
 
-  findAll() {
-    return `This action returns all chapters`;
+  findAll(): Promise<Chapter[]> {
+    return this.chaptersRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} chapter`;
+  async findOne(id: number): Promise<Chapter> {
+    const chapter = await this.chaptersRepository.findOne({where:{id}});
+    if (!chapter) {
+      throw new NotFoundException(`Chapter with ID ${id} not found`);
+    }
+    return chapter;
   }
 
-  update(id: number, updateChapterDto: UpdateChapterDto) {
-    return `This action updates a #${id} chapter`;
+  async update(id: number, updateChapterDto: UpdateChapterDto): Promise<Chapter> {
+    const chapter = await this.findOne(id);
+    Object.assign(chapter, updateChapterDto);
+    return this.chaptersRepository.save(chapter);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} chapter`;
+  async remove(id: number): Promise<void> {
+    const chapter = await this.findOne(id);
+    await this.chaptersRepository.remove(chapter);
   }
 }
