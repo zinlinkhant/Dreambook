3285fcb36800ec57e2cadaea65060639f5f3fa3e
Zin Lin Khant <zlkzlkzlk1122@gmail.com>
2024-05-28 20:06:24 +0630

    28 may 8pm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,47 +5,25 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { User } from './users/entities/user.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { BooksModule } from './books/books.module';
-import { Book } from './books/entities/book.entity';
 import { CategoriesModule } from './categories/categories.module';
-import { Category } from './categories/entities/category.entity';
 import { ChaptersModule } from './chapters/chapters.module';
-import { Chapter } from './chapters/entities/chapter.entity';
 import { ChapterProgressModule } from './chapter-progress/chapter-progress.module';
-import { ChapterProgress } from './chapter-progress/entities/chapter-progress.entity';
 import { FavouriteModule } from './favourite/favourite.module';
-import { Favourite } from './favourite/entities/favourite.entity';
 import { InterestedCategoryModule } from './interested-category/interested-category.module';
-import { InterestedCategory } from './interested-category/entities/interested-category.entity';
 import { CommentsModule } from './comments/comments.module';
-import { Comment } from './comments/entities/comment.entity';
-import { ModeEnum } from './utils/mode.enum';
-const sslReject = process.env.MODE === ModeEnum.Production ? {
-  ssl: {
-    rejectUnauthorized: false,
-  },
-} : null;@Module({
+import config from './typeorm/typeorm.config';
+
+
+
+@Module({
+  
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true, // Ensure that ConfigModule is globally available
+      isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
-        entities: [User,Book,Category,Chapter,ChapterProgress,Favourite,InterestedCategory,Comment],
-        ...sslReject,
-        synchronize: false, 
-      }),
-      inject: [ConfigService],
-    }),
+   TypeOrmModule.forRoot(config),
     AuthModule,
     UsersModule,
     BooksModule,
---
+++ src/typeorm/typeorm.config.ts
@@ -0,0 +1,30 @@
+import { Book } from "src/books/entities/book.entity";
+import { Category } from "src/categories/entities/category.entity";
+import { ChapterProgress } from "src/chapter-progress/entities/chapter-progress.entity";
+import { Chapter } from "src/chapters/entities/chapter.entity";
+import { Comment } from "src/comments/entities/comment.entity";
+import { Favourite } from "src/favourite/entities/favourite.entity";
+import { InterestedCategory } from "src/interested-category/entities/interested-category.entity";
+import { User } from "src/users/entities/user.entity";
+import { ModeEnum } from "src/utils/mode.enum";
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+const sslReject = process.env.MODE === ModeEnum.Production ? {
+  ssl: {
+    rejectUnauthorized: false,
+  },
+} 
+: null;
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [
+    User, Book, Category, Chapter, ChapterProgress, Favourite, InterestedCategory, Comment
+  ],
+  ...sslReject,
+  synchronize: true,
+};
+export default config;
