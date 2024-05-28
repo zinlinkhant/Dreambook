f269bba2e7405e1e36a1666a1a58dcbb18079e0e
Zin Lin Khant <zlkzlkzlk1122@gmail.com>
2024-05-28 14:23:20 +0630

    28 may 2:23

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableVersioning({
+    type: VersioningType.URI
+  });
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    transform: true
+  }));
+
+  await app.listen(process.env.PORT || 3000);
 }
-bootstrap(); 
+bootstrap();
