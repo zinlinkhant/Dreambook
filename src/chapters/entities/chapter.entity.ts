1298b90715056a2978f1715cb4233b2da50b99a9
Zin Lin Khant <zlkzlkzlk1122@gmail.com>
2024-06-02 23:04:48 +0630

    chapter cascade

--- src/chapters/entities/chapter.entity.ts
+++ src/chapters/entities/chapter.entity.ts
@@ -24,7 +24,7 @@ export class Chapter {
   @Column({ type: 'boolean', default: true })
   status: boolean;
 
-  @ManyToOne(() => Book, (book) => book.chapters)
+  @ManyToOne(() => Book, (book) => book.chapters,{ onDelete: 'CASCADE' })
   @JoinColumn({ name: 'bookId' })
   book: Book;
 
