20997ebdb0ee2c2a4a82c4743dce81d54e0bc391
Zin Lin Khant <zlkzlkzlk1122@gmail.com>
2024-06-06 12:00:00 +0630

    comment update updated

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../users/entities/user.entity';
@@ -33,11 +33,13 @@ export class CommentsService {
 
   async update(id: number, updateCommentDto: UpdateCommentDto, user: User): Promise<Comment> {
     const comment = await this.commentsRepository.findOne({ where: { id, userId: user.id } });
-
+    const userId = user.id
     if (!comment) {
       throw new NotFoundException(`Comment with ID ${id} not found or you do not own this comment`);
     }
-
+    if(comment.userId !== userId){
+      throw new UnauthorizedException("you do not own this comment")
+    }
     Object.assign(comment, updateCommentDto);
     return this.commentsRepository.save(comment);
   }
