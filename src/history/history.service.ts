3c4b7749f7afe03b99144b8a8cc87e6b853aa926
Zin Lin Khant <zlkzlkzlk1122@gmail.com>
2024-07-09 16:37:10 +0630

    history

--- src/history/history.service.ts
+++ src/history/history.service.ts
@@ -4,25 +4,43 @@ import { Repository } from 'typeorm';
 import { CreateHistoryDto } from './dto/create-history.dto';
 import { History } from './entities/history.entitiy';
 import { Book } from 'src/books/entities/book.entity';
-import { IPaginationOptions, paginate, Pagination } from 'nestjs-typeorm-paginate';
+import {
+  IPaginationOptions,
+  paginate,
+  Pagination,
+} from 'nestjs-typeorm-paginate';
 
 @Injectable()
 export class HistoryService {
-      constructor(
+  constructor(
     @InjectRepository(History)
     private readonly historyRepository: Repository<History>,
     @InjectRepository(Book)
     private readonly bookRepository: Repository<Book>,
   ) {}
 
-  async create(createHistoryDto: CreateHistoryDto,userId){
-    const bookId = (await this.bookRepository.findOne({where:{slug:createHistoryDto.bookSlug}})).id
-    const history = this.historyRepository.create({bookId,userId});
+  async create(createHistoryDto: CreateHistoryDto, userId) {
+    const bookId = (
+      await this.bookRepository.findOne({
+        where: { slug: createHistoryDto.bookSlug },
+      })
+    ).id;
+    const prevHistory = await this.historyRepository.findOne({
+      where: { userId: userId, bookId: bookId },
+    });
+    if (prevHistory) {
+      await this.historyRepository.remove(prevHistory);
+    }
+    const history = this.historyRepository.create({ bookId, userId });
     return this.historyRepository.save(history);
   }
 
-  async findAllByUser(userId: number, options: IPaginationOptions): Promise<Pagination<History>> {
-    const queryBuilder = this.historyRepository.createQueryBuilder('history')
+  async findAllByUser(
+    userId: number,
+    options: IPaginationOptions,
+  ): Promise<Pagination<History>> {
+    const queryBuilder = this.historyRepository
+      .createQueryBuilder('history')
       .leftJoinAndSelect('history.user', 'user')
       .leftJoinAndSelect('history.book', 'book')
       .leftJoinAndSelect('book.category', 'category')
